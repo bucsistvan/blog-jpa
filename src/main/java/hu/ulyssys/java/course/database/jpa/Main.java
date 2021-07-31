5cb85dbf222b177da56287fae7d37331286f76d4
bucsistvan <bucsistvan99@gmail.com>
2021-07-31 13:35:06 +0200

    Added blogpost's functionality to the project

--- src/main/java/hu/ulyssys/java/course/database/jpa/Main.java
+++ src/main/java/hu/ulyssys/java/course/database/jpa/Main.java
@@ -1,7 +1,10 @@
 package hu.ulyssys.java.course.database.jpa;
 
 import hu.ulyssys.java.course.database.jpa.dao.AuthorDAO;
+import hu.ulyssys.java.course.database.jpa.dao.BlogPostDAO;
 import hu.ulyssys.java.course.database.jpa.entity.Author;
+import hu.ulyssys.java.course.database.jpa.entity.BlogPost;
+import hu.ulyssys.java.course.database.jpa.entity.Category;
 
 import java.util.Date;
 
@@ -18,6 +21,20 @@ public class Main {
         authorDAO.save(author);
         authorDAO.findAll().forEach(author1 -> {
             System.out.println(author1.getId() + " " + author1.getFirstName() + " " +author1.getLastName());
+        });
+
+
+        BlogPostDAO blogPostDAO = new BlogPostDAO();
+        BlogPost blogPost = new BlogPost();
+        blogPost.setTitle("Felhők tánca");
+        blogPost.setCategory(Category.valueOf("JAVA"));
+        blogPost.setContent("ascacascascdvsdasasc");
+        blogPost.setCreatedDate(new Date(System.currentTimeMillis()));
+        blogPost.setLastModifiedDate(new Date(System.currentTimeMillis()));
+        blogPost.setPublishedDate(new Date(System.currentTimeMillis()));
+        blogPostDAO.save(blogPost);
+        blogPostDAO.findAll().forEach(blogPost1 -> {
+            System.out.println(blogPost1.getId() + " " + blogPost1.getTitle() + " " +blogPost1.getCategory());
         });
     }
 }
--- src/main/java/hu/ulyssys/java/course/database/jpa/entity/BlogPost.java
+++ src/main/java/hu/ulyssys/java/course/database/jpa/entity/BlogPost.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table
+@Table(name = "blog_post")
 public class BlogPost {
 
     @Id
